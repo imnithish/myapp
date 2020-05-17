e3c2b51ff8a3d56e2ea415602f29537e556333dd
imnithish <imnithish@live.com>
2020-05-17 23:33:00 +0530

    text

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,34 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-
       appBar: AppBar(
-        title: Text("Hello"),
+        title: Text(
+            "Hello",
+        style: TextStyle(
+          fontFamily: 'ProductSans',
+          fontWeight: FontWeight.bold,
+          color: Colors.white
+        ),),
         centerTitle: true,
+        backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text("hey"),
+        child: Text(
+          "Hey Nitheesh",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey,
+            letterSpacing: 2.0,
+            fontFamily: 'ProductSans',
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        child: Text("click")
+        onPressed: () {},
+        child: Text("click"),
+        backgroundColor: Colors.redAccent,
       ),
     ),
   ));
 }
-
