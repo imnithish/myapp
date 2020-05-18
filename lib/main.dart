dfffccb8d01c8fd43cd955712ede4f8c07ffcc22
imnithish <imnithish@live.com>
2020-05-19 02:07:53 +0530

    card app 2

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class Card extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black87,
       appBar: AppBar(
         title: Text(
           "CARD APP",
@@ -15,10 +16,94 @@ class Card extends StatelessWidget {
               fontWeight: FontWeight.bold,
               color: Colors.white),
         ),
+        elevation: 2.0,
         centerTitle: true,
-        backgroundColor: Colors.red[600],
+        backgroundColor: Colors.deepOrange,
       ),
-
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/two.jpg'),
+                radius: 100,
+              ),
+            ),
+            Divider(
+              height: 60,
+              color: Colors.grey[600],
+            ),
+            Text(
+              'NAME:',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'ProductSans',
+                  color: Colors.white,
+                  letterSpacing: 1.0),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Nitheesh Ag',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'ProductSans',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 1.0),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              'CURRENT LEVEL:',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'ProductSans',
+                  color: Colors.white,
+                  letterSpacing: 1.0),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '8',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'ProductSans',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 1.0),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  'IMNITHISH@LIVE.COM',
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontFamily: 'ProductSans',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      letterSpacing: 1.0),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
