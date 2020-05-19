6d2a9f95b911adece2f949e15430b9659b24a2bb
imnithish <imnithish@live.com>
2020-05-20 02:45:04 +0530

    Stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,141 +1,59 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
-void main() => runApp(MaterialApp(home: Card()));
+void main() =>
+    runApp(MaterialApp(
+      home: QuoteList(),
+    ));
 
-class Card extends StatefulWidget {
+class QuoteList extends StatefulWidget {
   @override
-  _CardState createState() => _CardState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class _CardState extends State<Card> {
-  int level = 0;
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(text: 'live a life', author: 'nitheesh'),
+    Quote(text: 'hi there how are you', author: 'ramesh'),
+    Quote(text: 'i love quotes', author: 'sumesh')
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black87,
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text(
-          "CARD APP",
-          style: TextStyle(
-              fontFamily: 'ProductSans',
-              fontWeight: FontWeight.bold,
-              color: Colors.white),
-        ),
-        elevation: 2.0,
+        title: Text('Quotes List'),
         centerTitle: true,
-        backgroundColor: Colors.deepOrange,
+        backgroundColor: Colors.redAccent,
       ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/two.jpg'),
-                radius: 50,
-              ),
-            ),
-            Center(
-              child: Divider(
-                height: 60,
-                color: Colors.grey[600],
-              ),
-            ),
-            Text(
-              'NAME:',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'ProductSans',
-                  color: Colors.white,
-                  letterSpacing: 1.0),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              'Nitheesh Ag',
-              style: TextStyle(
-                  fontSize: 20,
-                  fontFamily: 'ProductSans',
-                  fontWeight: FontWeight.bold,
-                  color: Colors.amberAccent[200],
-                  letterSpacing: 1.0),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              'CURRENT LEVEL:',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'ProductSans',
-                  color: Colors.white,
-                  letterSpacing: 1.0),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              '$level',
-              style: TextStyle(
-                  fontSize: 20,
-                  fontFamily: 'ProductSans',
-                  fontWeight: FontWeight.bold,
-                  color: Colors.amberAccent[200],
-                  letterSpacing: 1.0),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Row(
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.email),
-                  tooltip: 'reset',
-                  color: Colors.white,
-                  onPressed: () {
-                    reset();
-                  },
-                ),
-                SizedBox(
-                  width: 5,
-                ),
-                Text(
-                  'IMNITHISH@LIVE.COM',
-                  style: TextStyle(
-                      fontSize: 15,
-                      fontFamily: 'ProductSans',
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      letterSpacing: 1.0),
-                )
-              ],
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          update();
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.deepOrange,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: quotes
+            .map((quotes) =>
+            Text('quote: ${quotes.text}\nauthor: ${quotes.author}'))
+            .toList(),
       ),
     );
   }
 
-  void update() {
-    setState(() {
-      level += 1;
-    });
-  }
-
-  void reset() {
-    setState(() {
-      level = 0;
-    });
+  Widget quotesTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Column(
+        children: <Widget>[
+          Text(
+            quote.text,
+          ),
+          SizedBox(
+            height: 6,
+          ),
+          Text(
+              quote.author
+          )
+        ],
+      ),
+    );
   }
 }
